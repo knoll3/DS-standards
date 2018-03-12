dfb15443ae012137f3a68031458356322a7e8c4f
knoll3 <noll.kyle@gmail.com>
2018-03-12 21:53:55 +0000

    include code to import csv into db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,14 @@
-Framing.create desc: "tangent vertical", coserv: 'va1', oncor: '108-205', tnmp: 'OH-6-100', trinity_valley: 'va1'
-Framing.create desc: "deadend vertical", coserv: 'va5', oncor: '108-235', tnmp: 'OH-6-120', trinity_valley: 'va5'
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'framings.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Framing.new
+  t.desc = row['desc']
+  t.coserv = row['coserv']
+  t.oncor = row['oncor']
+  t.tnmp = row['tnmp']
+  t.trinity_valley = row['trinity_valley']
+  t.save
+end
+puts 'db saved'
