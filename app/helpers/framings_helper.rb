58ad1aee63d6019d58ca6371bc3fab2b5dca599f
knoll3 <noll.kyle@gmail.com>
2018-03-22 12:36:25 +0000

    Modify pdf links

--- app/helpers/framings_helper.rb
+++ app/helpers/framings_helper.rb
@@ -12,6 +12,16 @@ module FramingsHelper
     "https://framing-helper-bucket-20180320.s3.amazonaws.com/#{url}"
   end
   
+  def build_standards_href file_path, page_href = nil
+    href = ''
+    bucket = "framing-helper-bucket-20180320"
+    href = "https://#{bucket}.s3.amazonaws.com/standards/#{file_path}"
+    if page_href
+      href += page_href
+    end
+    return href
+  end
+  
   def get_name_from url
     url.scan(/(.+)_/)[0][0].sub('_',' ').upcase
   end
