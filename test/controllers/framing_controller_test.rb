4709e2bb004687457ac9cbb8afad9e30889b5f31
Knoll <noll.kyle@gmail.com>
2018-03-22 18:35:58 -0500

    Start testing

--- test/controllers/framing_controller_test.rb
+++ test/controllers/framing_controller_test.rb
@@ -1,14 +1,10 @@
 require 'test_helper'
 
 class FramingControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get framing_index_url
-    assert_response :success
-  end
 
-  test "should get show" do
-    get framing_show_url
+  test "should load root" do
+    get root_path
     assert_response :success
+    assert_select 'title', 'FramingHelper'
   end
-
 end
