a067e245e871acff7e215c010a0627b0a9048db2
knoll3 <noll.kyle@gmail.com>
2018-03-26 20:14:45 +0000

    Update for heroku

--- build_app.rb
+++ build_app.rb
@@ -2,4 +2,7 @@
 `git commit -m "Update for heroku"`
 `git push`
 `git push heroku`
+`heroku pg:reset DATABASE --confirm infinite-oasis-23086`
+`heroku run rails db:migrate`
+`heroku run rails db:seed`
 
