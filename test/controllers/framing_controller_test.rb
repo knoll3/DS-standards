cfb75125d08cbcc57bcc5f1a686e06aa479090f9
Knoll <noll.kyle@gmail.com>
2018-03-31 11:44:27 -0500

    Get tests working

--- test/controllers/framing_controller_test.rb
+++ test/controllers/framing_controller_test.rb
@@ -17,6 +17,33 @@ class FramingControllerTest < ActionDispatch::IntegrationTest
                               created_at: "2018-03-26 14:10:45", 
                               updated_at: "2018-03-26 14:10:48", 
                               page: "23")
+    @image   = Image.create(  id: 2, 
+                              url: "oncor_108-205.jpg", 
+                              framing_id: 1, 
+                              created_at: "2018-03-26 14:10:45", 
+                              updated_at: "2018-03-26 14:10:48", 
+                              page: "23")
+    @image   = Image.create(  id: 3, 
+                              url: "tnmp_OH-6-100.jpg", 
+                              framing_id: 1, 
+                              created_at: "2018-03-26 14:10:45", 
+                              updated_at: "2018-03-26 14:10:48", 
+                              page: "23")
+    @image   = Image.create(  id: 4, 
+                              url: "trinity_valley_va1.jpg", 
+                              framing_id: 1, 
+                              created_at: "2018-03-26 14:10:45", 
+                              updated_at: "2018-03-26 14:10:48", 
+                              page: "23")
+    @company = Company.create(name: 'coserv',
+                              visible: true)
+    @company = Company.create(name: 'oncor',
+                              visible: true)
+    @company = Company.create(name: 'tnmp',
+                              visible: true)
+    @company = Company.create(name: 'trinity_valley',
+                              visible: true)
+
   end
   
   test "should load root" do
@@ -28,12 +55,16 @@ class FramingControllerTest < ActionDispatch::IntegrationTest
     assert_select "td", "108-205"
     assert_select "td", "OH-6-100"
   end
-  
+
   test "pdf link should exist" do
     get root_path
     domain = 'https://framing-helper-bucket-20180320.s3.amazonaws.com'
     assert_select 'a[href=?]', "#{domain}/standards/coserv.pdf"
   end
-  
+ 
+  test "should only load visible companies" do
+    company = Company.find_by(name: 'tnmp')  
+    
+  end 
   
 end
