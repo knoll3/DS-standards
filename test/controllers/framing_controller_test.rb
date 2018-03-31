8f213a42aedd3ecf5b2aed33055adb46844f80c0
Knoll <noll.kyle@gmail.com>
2018-03-31 12:36:19 -0500

    Add more controller tests

--- test/controllers/framing_controller_test.rb
+++ test/controllers/framing_controller_test.rb
@@ -63,8 +63,45 @@ class FramingControllerTest < ActionDispatch::IntegrationTest
   end
  
   test "should only load visible companies" do
-    company = Company.find_by(name: 'tnmp')  
-    
+    # Remove Oncor
+    company = Company.find_by(name: 'oncor')
+    company.visible = false
+    company.save    
+    get root_path
+    assert_select 'th', 'COSERV'
+    assert_select 'td', 'VA1'
+    assert_select 'th', {count: 0, text: 'ONCOR'}
+    assert_select 'td', {count: 0, text: '108-205'}
+    assert_select 'th', 'TNMP'
+    assert_select 'td', 'OH-6-100'
+    assert_select 'th', 'TRINITY VALLEY'
+    img_name = 'coserv_va1.jpg'
+    assert_select 'img[src=?]', "https://framing-helper-bucket-20180320.s3.amazonaws.com/#{img_name}", count: 1
+    img_name = 'oncor_108-205.jpg'
+    assert_select 'img[src=?]', "https://framing-helper-bucket-20180320.s3.amazonaws.com/#{img_name}", count: 0
+    img_name = 'tnmp_OH-6-100.jpg'
+    assert_select 'img[src=?]', "https://framing-helper-bucket-20180320.s3.amazonaws.com/#{img_name}", count: 1
+    img_name = 'trinity_valley_va1.jpg'
+    assert_select 'img[src=?]', "https://framing-helper-bucket-20180320.s3.amazonaws.com/#{img_name}", count: 1
+
+    # Remove Coserv
+    company = Company.find_by(name: 'coserv')
+    company.visible = false
+    company.save    
+    get root_path
+    assert_select 'th', {count: 0, text: 'COSERV'}
+    assert_select 'td', {count: 1, text: 'VA1'}
+    assert_select 'th', {count: 0, text: 'ONCOR'}
+    assert_select 'th', 'TNMP'
+    assert_select 'th', 'TRINITY VALLEY'
+    img_name = 'coserv_va1.jpg'
+    assert_select 'img[src=?]', "https://framing-helper-bucket-20180320.s3.amazonaws.com/#{img_name}", count: 0
+    img_name = 'oncor_108-205.jpg'
+    assert_select 'img[src=?]', "https://framing-helper-bucket-20180320.s3.amazonaws.com/#{img_name}", count: 0
+    img_name = 'tnmp_OH-6-100.jpg'
+    assert_select 'img[src=?]', "https://framing-helper-bucket-20180320.s3.amazonaws.com/#{img_name}", count: 1
+    img_name = 'trinity_valley_va1.jpg'
+    assert_select 'img[src=?]', "https://framing-helper-bucket-20180320.s3.amazonaws.com/#{img_name}", count: 1
   end 
   
 end
